b7493609abae93463a1c4e275802625628edb6fb
Jay Patel <81924044+jayp822@users.noreply.github.com>
2021-09-04 15:38:34 -0400

    Update SentenceToNumberOfWords.java

--- SentenceToNumberOfWords.java
+++ SentenceToNumberOfWords.java
@@ -1,25 +1,25 @@
-package WordsInString;
-
 import javax.swing.JOptionPane;
 
 public class SentenceToNumberOfWords {
 
 	public static void main(String[] args) {
 		String sentence = JOptionPane.showInputDialog(null, "Enter a sentence with correct grammar: ");
-		int words = wordCount(sentence);
+		int words = numberOfWords(sentence);
 		JOptionPane.showInternalMessageDialog(null, "There are " + words + " words in your sentence.");
 		System.exit(0);
 
 	}
 
-	public static int wordCount(String str) {
-		int count = 1;
-		char[] spaces = str.toCharArray();
-		for (int i = 0; i < spaces.length; i++) {
-			if ((spaces[i] == ' ' && Character.isLetterOrDigit(spaces[i + 1]))){
-				count++;
-			}
+	public static int numberOfWords(String str) {
+
+		str = str.trim();
+		String[] tokenize = str.split(" ");
+		int count = 0;
+		for (@SuppressWarnings("unused")
+		String words : tokenize) {
+			count++;
 		}
 		return count;
 	}
+
 }
